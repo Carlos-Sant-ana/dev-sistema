b3cee9ffb6e1e326cbe9e8ed448f4aedede49309
Prof. Holanda <adriano.holanda@fafram.com.br>
2020-09-01 20:06:13 -0300

    Ponto declarado

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
-
-pub struct Tipo {
-  var1: TipoVar1,
-  var2: TipoVar2,
-  ...
-  varn: TipoVarN
+/// Estrutura Ponto
+/// x: coordenada x
+/// y: coordenada y
+pub struct Ponto {
+    x: i64,
+    y: i64,
 }
 
-impl Tipo {
-  pub fn new(valor1: TipoVar1, valor2, TipoVar2, ..., valorn: TipoVarN) -> Self {
-    Tipo {
-      var1: valor1,
-      var2: valor2,
-      ...
-      varn: valorn,
+/// Cria a estrutura ponto.
+impl Ponto {
+    pub fn new(x: i64, y: i64) -> Self {
+        Ponto {
+            x,
+            y,
+        }
     }
-  }
 }
 
 fn main() {
-    let tipo = Tipo::new(valor1, valor2, ..., valorn);
-    println!("{:?}", tipo);
+
+    //    println!("{:?}", tipo);
 }
 
 #[cfg(test)]
 mod tests {
+    use self::Ponto;
     #[test]
     fn eita_funciona() {
-      // Implementar um teste usando o tipo Tipo
-        assert_eq!(2 + 2, 4);
+        let p = Ponto.new(2, 3);
+
+        assert_eq!(p.x, 2);
     }
 }
