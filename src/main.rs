1113d3e2b296a15a2d894c9c56b0d47a3b899f65
Prof. Holanda <adriano.holanda@fafram.com.br>
2020-09-15 11:45:38 -0300

    comment rectangle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-/// Estrutura Ponto
+/// Representa a figura geométrica ponto onde
 /// x: coordenada x
 /// y: coordenada y
 #[allow(dead_code)]
@@ -7,14 +7,21 @@ struct Ponto {
     y: i64,
 }
 
+/// Representa a figura geométrica retângulo onde
+/// piso_esquerda: é o ponto mais a esquerda e próximo do piso,
+/// teto_direita: é o ponto mais a direito e próximo do teto (topo).
 #[allow(dead_code)]
 struct Retangulo {
     piso_esquerda: Ponto,
     teto_direita: Ponto,
 }
 
+// Implementações para o Retangulo.
 #[allow(dead_code)]
 impl Retangulo {
+    /// Calcula a área do retangulo pela multiplicação 
+    /// das diferenças entre as coordenadas x e y.
+    /// Retorna um inteiro como resultado do cálculo da área.
     fn area(self) -> i64 {
         let dx = self.piso_esquerda.x - self.teto_direita.x;
         let dy = self.piso_esquerda.y - self.teto_direita.y;
